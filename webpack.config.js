67a894cc4b7f8b0339c70050c57c981888ae3a9d
Shawn Roman <avahaulick@gmail.com>
2026-01-31 21:51:23 -0700

    Complete npm INstall for Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,46 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/pages/index.js",
+  entry: {
+    main: "./src/pages/index.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
     publicPath: "",
   },
+
   mode: "development",
+  devtool: "inline-source-map",
+  stats: "errors-only",
   devServer: {
     static: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
     open: true,
+    liveReload: true,
+    hot: false,
   },
+  target: ["web", "es5"],
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
